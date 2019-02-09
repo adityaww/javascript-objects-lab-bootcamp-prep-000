fda61da151e518ac349129b01d2a9eb9f99a4723
Aditya Waruwu <aditya.william96@gmail.com>
2019-02-09 05:42:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var obj = {};
   var newObj = Object.assign({}, obj);
   delete newObj[key];
   return newObj;
